5caf6cea18be6532ba51d34426f927774425019f
SilpaPrasad08 <silpa.prasad.me@gmail.com>
2023-10-17 21:40:26 +0530

    first commit

---
+++ src/app/components/Charts.js
@@ -0,0 +1,134 @@
+'use client'
+import BarChartPlot from "./BarChartPlot";
+import PieChartPlot from "./PieChartPlot";
+const Charts = () => {
+  return (
+    <>
+      <section>
+        <div className="flex m-4 gap-2">
+          <div className="flex-1 px-2 justify-center w-16 bg-gray-700 shadow rounded h-300px">
+            <div className="">
+              <p className="text-gray-900 font-bold">Total returns</p>
+              <p className="py-4 font-bold">$30,000 </p>
+              <p className="text-green-300">+34.5%</p>
+            </div>
+          </div>
+          <div className="flex-1 px-2 justify-center w-16 bg-gray-700 shadow rounded max-h-300px">
+            <div className="">
+              <p className="text-gray-900 font-bold">Total sales</p>
+              <p className="py-4 font-bold">$30,000 </p>
+              <p className="text-green-300">+34.5%</p>
+            </div>
+          </div>
+          <div className="flex-1 px-2 justify-center w-16  bg-gray-700 shadow rounded max-h-300px">
+            <div className="">
+              <p className="text-gray-900 font-bold">Total subscriptions</p>
+              <p className="py-4 font-bold">$30,000 </p>
+              <p className="text-green-300">+34.5%</p>
+            </div>
+          </div>
+          <div className="flex-1 px-2 justify-center w-16  bg-gray-700 shadow rounded h-300px">
+            <div className="">
+              <p className="text-gray-900 font-bold">Total returns</p>
+              <p className="py-4 font-bold ">$30,000 </p>
+              <p className="text-green-300">+34.5%</p>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="flex my-4 px-4 gap-3">
+      <div className="w-3/4 h-[300px] bg-gray-700 rounded"><BarChartPlot/></div>
+
+      <div className=" w-1/4 h-[300px] bg-gray-700 rounded"><PieChartPlot/></div>
+      </section>
+
+      <section className="flex my-4 px-4 gap-2">
+
+      <div className=" w-full h-[250px] bg-gray-700 rounded">
+    <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+                <th scope="col" class="px-6 py-3">
+                    Product name
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Stock
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Price
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Total Sales
+                </th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    Apple MacBook Pro 17"
+                </th>
+                <td class="px-6 py-4">
+                    Silver
+                </td>
+                <td class="px-6 py-4">
+                    $2999
+                </td>
+                <td class="px-6 py-4">
+                    120
+                </td>
+            </tr>
+            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    Microsoft Surface Pro
+                </th>
+                <td class="px-6 py-4">
+                    White
+                </td>
+                <td class="px-6 py-4">
+                    $4000
+                </td>
+                <td class="px-6 py-4">
+                    150
+                </td>
+            </tr>
+            <tr class="bg-white dark:bg-gray-800">
+                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    Magic Mouse 2
+                </th>
+                <td class="px-6 py-4">
+                    Black
+                </td>
+                <td class="px-6 py-4">
+                    $99
+                </td>
+                <td class="px-6 py-4">
+                    100
+                </td>
+            </tr>
+            <tr class="bg-white dark:bg-gray-800">
+                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    Magic Mouse 2
+                </th>
+                <td class="px-6 py-4">
+                    Black
+                </td>
+                <td class="px-6 py-4">
+                    $99
+                </td>
+                <td class="px-6 py-4">
+                    100
+                </td>
+            </tr>
+        </tbody>
+    </table>
+</div>
+
+      </section>
+
+      
+    </>
+  );
+};
+
+export default Charts;
